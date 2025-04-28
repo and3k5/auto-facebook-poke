16802ff7a00a30ae10513b96a06e00e216aefcb5
Anders K <and3k5@users.noreply.github.com>
2025-04-28 14:24:58 +0200

    Update script

--- auto-facebook-poke.user.js
+++ auto-facebook-poke.user.js
@@ -2,7 +2,10 @@
 // @name         Facebook autoclick
 // @namespace    http://github.com/and3k5
 // @version      2025-04-28
-// @description  Automatic facebook poke
+// @description  Automatic facebook poke. Clicks on poke back every second.
+// @homepageURL  https://github.com/and3k5/auto-facebook-poke
+// @supportURL   https://github.com/and3k5/auto-facebook-poke/issues
+// @copyright    2025 And3k5
 // @author       And3k5
 // @match        https://www.facebook.com/pokes*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -12,7 +15,7 @@
 
 (function () {
     "use strict";
-    const header = document
+    const button = document
         .evaluate(
             "//body//*//span[text()[contains(.,'Pokes')]]",
             document.body,
@@ -20,31 +23,66 @@
             XPathResult.FIRST_ORDERED_NODE_TYPE,
             null
         )
-        .singleNodeValue.appendChild(document.createElement("a"));
-    header.innerText = "Start auto poke";
-    header.href = "javascript:void(0)";
-    header.style.color = "#a31a1a";
-    header.style.fontSize = "12pt";
-    header.style.verticalAlign = "baseline";
-    header.style.overflow = "visible";
-    header.style.marginLeft = "20px";
-    header.style.background = "#a3b0b7";
-    header.style.borderRadius = "50px";
-    header.style.padding = "5px";
-    header.style.lineHeight = "3";
-    header.addEventListener("click", () => {
-        setInterval(
-            () =>
-                document
-                    .evaluate(
-                        "//*[text()[contains(.,'Poke Back')]]",
-                        document.body,
-                        null,
-                        XPathResult.FIRST_ORDERED_NODE_TYPE,
-                        null
-                    )
-                    .singleNodeValue?.click(),
-            1000
-        );
+        .singleNodeValue.appendChild(document.createElement("button"));
+    button.textContent = "_";
+    button.type = "button";
+    button.style.fontSize = "12pt";
+    button.style.verticalAlign = "baseline";
+    button.style.overflow = "visible";
+    button.style.borderRadius = "50px";
+    button.style.margin = "6px";
+    button.style.marginLeft = "20px";
+    button.style.cursor = "pointer";
+
+    const spinner = button.appendChild(document.createElement("span"));
+    spinner.textContent = "|";
+    const animation = spinner.animate(
+        [
+            { transform: "rotate(0)" },
+            { transform: "rotate(360deg)" },
+        ],
+        {
+            duration: 1000,
+            iterations: 1,
+        }
+    );
+
+    let intervalId = null;
+    const pokeBack = () =>
+        document
+            .evaluate(
+                "//*[text()[contains(.,'Poke Back')]]",
+                document.body,
+                null,
+                XPathResult.FIRST_ORDERED_NODE_TYPE,
+                null
+            )
+            .singleNodeValue?.click();
+    const setActiveState = () => {
+        button.childNodes[0].textContent = "Stop auto poke";
+        spinner.style.display = "inline-block";
+    };
+    const start = () => {
+        if (intervalId != null) return;
+        intervalId = setInterval(() => {
+            pokeBack();
+            animation.currentTime = 0;
+        }, 1000);
+        setActiveState();
+    };
+    const setIdleState = () => {
+        button.childNodes[0].textContent = "Start auto poke";
+        spinner.style.display = "none";
+    };
+    const stop = () => {
+        if (intervalId == null) return;
+        clearInterval(intervalId);
+        intervalId = null;
+        setIdleState();
+    };
+    setIdleState();
+    button.addEventListener("click", () => {
+        if (intervalId == null) return start();
+        else stop();
     });
 })();
